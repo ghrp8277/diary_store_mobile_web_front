50d8e8ac89988d7e33c8aa5dfa93e5ec23869652
LEE YEJI <79579125+LEEYEJI501@users.noreply.github.com>
2022-08-15 19:55:46 +0900

    Update 메인페이지 라우터 추가
    author : Lee yeji
    
    - udpate HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!--메뉴바-->
+    <div class="menubar-container">
+      <button class="leftButton" @click="menuButton">
+        <font-awesome-icon icon="fa-bars" style="width: 50%; height: auto" />
+      </button>
+      <span class="title">{{ msg }}</span>
+      <div>
+        <button class="rightButton" @click="searchButton">
+          <font-awesome-icon
+            icon="fa-magnifying-glass"
+            style="width: 50%; height: auto"
+          />
+        </button>
+      </div>
+    </div>
+
+    <router-view />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Vue from 'vue';
 
 export default Vue.extend({
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  name: 'MainCategory, EmoticonCategory',
+  data() {
+    return {
+      msg: '이모티콘 스토어',
+    };
+  },
+  methods: {
+    // 메뉴버튼 클릭 함수
+    menuButton() {
+      console.log('menubutton click');
+    },
+    // 검색버튼 클릭 함수
+    searchButton() {
+      this.$router.push({ path: 'search' });
+    },
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.menubar-container {
+  height: 35px;
+  margin: 15px 10px;
+  display: grid;
+  grid-template-columns: 0.5fr 2fr 0.5fr;
+}
+button {
+  width: 40px;
+  height: 100%;
+  background: white;
+  border: 0px;
+  padding: 0px;
+}
+.rightButton {
+  float: right;
+}
+.title {
+  font-size: 18px;
+  font-weight: bold;
+  line-height: 35px;
+}
+</style>
