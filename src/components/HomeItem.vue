a80121624ba5d37a8a7d033026b37542422413a4
LEE YEJI <79579125+LEEYEJI501@users.noreply.github.com>
2022-09-15 20:28:44 +0900

    Update 메인화면 수정
    author : Lee yeji

--- src/components/HomeItem.vue
+++ src/components/HomeItem.vue
@@ -1,14 +1,15 @@
+@@ -1,102 +1,164 @@
 <template>
   <div class="container">
-    <div class="list">
-      <div class="list-item">
-        <h2 class="list-tag"># 강아지 이모티콘</h2>
-        <div class="emoji-img">
-          <img src="@/assets/logo.png" alt="" />
-          <img src="@/assets/logo.png" alt="" />
-          <img src="@/assets/logo.png" alt="" />
-          <img src="@/assets/logo.png" alt="" />
-        </div>
+    <h2 class="tag"># 강아지 이모티콘</h2>
+    <div class="emoji">
+      <div class="img-list">
+        <img src="@/assets/logo.png" alt="" />
+        <img src="@/assets/logo.png" alt="" />
+        <img src="@/assets/logo.png" alt="" />
+        <img src="@/assets/logo.png" alt="" />
+        <img src="@/assets/logo.png" alt="" />
+        <img src="@/assets/logo.png" alt="" />
       </div>
     </div>
   </div>
@@ -49,54 +50,133 @@ export default defineComponent({
 }
 
 .list {
+  border: 1px solid #777;
+
   width: 100%;
   height: 200px;
   border: 1px solid lightgray;
   border-radius: 20px;
 }
 .list-item {
+  height: 100%;
+
   display: flex;
+
+  flex-direction: row;
+
+  flex-wrap: nowrap;
+
+  justify-content: center;
+
+  align-items: center;
+
+  border-radius: 5px;
 }
 .list-tag {
   line-height: inherit;
+}
+.tag {
+  font-size: 18px;
+  text-align: center;
+  text-indent: 15px;
+
   flex-grow: 0.5;
   text-align: left;
   margin: 0;
   padding-left: 15px;
+
+  min-width: 150px;
 }
 .emoji-img {
   height: 200px;
+}
+.emoji {
+  overflow-x: auto;
+  scroll-snap-type: none;
+}
+.img-list {
   flex-basis: 0;
   display: flex;
+
+  justify-content: space-around;
 }
+
 img {
   width: 150px;
   height: 150px;
+  width: 130px;
+  height: 130px;
 }
 
-/* 그리드 */
-.grid-container {
-  /**
-     * User input values.
-     */
-  --grid-layout-gap: 10px;
-  --grid-column-count: 4;
-  --grid-item--min-width: 200px;
+/* 미디어 쿼리 */
+@media all and (max-width: 1030px) {
+  .container {
+    //   flex-direction: column;
 
-  /**
-     * Calculated values.
-     */
-  --gap-count: calc(var(--grid-column-count) - 1);
-  --total-gap-width: calc(var(--gap-count) * var(--grid-layout-gap));
-  --grid-item--max-width: calc(
-    (100% - var(--total-gap-width)) / var(--grid-column-count)
-  );
+    margin: 0 10px;
 
-  display: grid;
-  grid-template-columns: repeat(
-    auto-fill,
-    minmax(max(var(--grid-item--min-width), var(--grid-item--max-width)), 1fr)
-  );
-  grid-gap: var(--grid-layout-gap);
+    width: calc(100% - 20px);
+
+    //   background-color: #777;
+  }
+  .emoji {
+    width: 100%;
+  }
+  .img-list {
+    // overflow-x: scroll;
+    // justify-content: flex-start;
+  }
+
+  img {
+    width: 100px;
+    height: 100px;
+  }
+
+  // img:nth-child(n + 5) {
+  //   display: block;
+  // }
+
+  .tag {
+    // flex: 1;
+
+    // width: 100%;
+
+    // text-align: left;
+
+    // font-size: 14px;
+
+    // font-weight: bold;
+
+    // color: #000;
+
+    margin: 0 10px;
+    flex-grow: 2;
+  }
 }
+
+// /* 미디어 쿼리 */
+// @media all and (max-width: 915px) {
+//   .container {
+//     overflow-x: scroll;
+//     overflow-y: hidden;
+
+//     height: 192px;
+//   }
+
+//   .tag {
+//     position: fixed;
+
+//     top: 120px;
+
+//     left: 10px;
+//   }
+// }
+
+// /* 미디어 쿼리 */
+// @media all and (max-width: 500px) {
+//   img {
+//     width: 50px;
+//     height: 50px;
+//   }
+// }
 </style>
