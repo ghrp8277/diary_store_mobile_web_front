1dff7393568a4a1d40ee54ece804ea7628f663f0
ghrp8277 <ghrp8277@naver.com>
2022-09-19 16:09:16 +0900

    UPDATE main tab 변경

--- src/views/MainTabView.vue
+++ src/views/MainTabView.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <CategoryTab />
 
-    <router-view />
+    <transition name="slide-fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -19,4 +21,19 @@ export default defineComponent({
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.slide-fade-enter {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 0.2s ease;
+}
+
+.slide-fade-leave-to {
+  transform: translateX(-10px);
+  opacity: 0;
+}
+</style>
