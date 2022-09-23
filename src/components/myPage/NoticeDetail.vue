c63ceb1d04de7a008d2dcaf9839f9547f9f20e1b
LEE YEJI <79579125+LEEYEJI501@users.noreply.github.com>
2022-09-23 23:22:25 +0900

    UPDATE service center 추가
    author : Lee yeji

---
+++ src/components/myPage/NoticeDetail.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container-notice">
+    <table class="table-notice">
+      <tr>
+        <td class="txt-tit">dsfasd</td>
+        <td class="txt-date">2021.09.02</td>
+      </tr>
+    </table>
+    <!-- <p><button v-on:click="show = !show">변경하기</button></p>
+    <transition>
+      <div v-if="show" key="a">TRUE</div>
+      <div v-else key="b">FALSE</div>
+    </transition> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+export default defineComponent({
+  name: 'NoticeDetail',
+  data() {
+    return {
+      show: true,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.container-notice {
+  background: white;
+  padding: 30px 100px;
+}
+.table-notice {
+  border-top: 1px solid lightgray;
+  border-collapse: collapse;
+  text-align: left;
+
+  th,
+  td {
+    border-bottom: 1px solid lightgray;
+    padding: 10px;
+
+    font-size: 14px;
+  }
+
+  .txt-tit {
+    width: 90%;
+  }
+  .txt-date {
+    width: 20%;
+  }
+}
+// .v-enter-active,
+// .v-leave-active {
+//   transition: opacity 1s;
+// }
+// .v-leave-active {
+//   position: absolute;
+// }
+// .v-enter,
+// .v-leave-to {
+//   opacity: 0;
+// }
+</style>
---
+++ src/views/myPage/ServiceCenterView.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container-serv">
+    <div class="area-menu">
+      <h2 class="txt-tit">고객센터</h2>
+      <ul>
+        <router-link :to="{ name: 'payment' }">
+          <li>공지사항</li>
+        </router-link>
+        <li>자주 묻는 질문</li>
+      </ul>
+      <!-- <button class="btn-notice">공지사항</button>
+      <button class="btn-faq">자주 묻는 질문</button> -->
+    </div>
+    <keep-alive>
+      <component v-bind:is="comp"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import NoticeDetail from '@/components/myPage/NoticeDetail.vue';
+import FAQDetail from '@/components/myPage/FAQDetail.vue';
+
+export default defineComponent({
+  components: { NoticeDetail, FAQDetail },
+  data() {
+    return {
+      comp: 'NoticeDetail',
+    };
+  },
+  setup() {
+    // changeComponent: function(componentName){
+    //   this.comp = componentName
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.container-serv {
+  background: white;
+}
+.area-menu {
+  background: #f5f5f5;
+  padding: 30px 100px;
+  text-align: left;
+
+  ul {
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    height: 30px;
+    margin: 0 10px 0 0;
+    padding: 0 15px;
+
+    border: 1px solid gray;
+    border-radius: 50px;
+    color: black;
+    background: 0;
+
+    font-size: 14px;
+    line-height: 29px;
+    cursor: pointer;
+    text-decoration: none;
+  }
+}
+.txt-tit {
+  margin: 0 0 25px 0;
+}
+</style>
