214b0f7bcbdfc9e096015072542e19f9c63da0c0
LEE YEJI <79579125+LEEYEJI501@users.noreply.github.com>
2022-09-29 01:10:41 +0900

    UPDATE : API 주석 추가
    author : Lee yeji

--- src/apis/buyer/index.ts
+++ src/apis/buyer/index.ts
@@ -1,12 +1,14 @@
 import { instance } from '@/apis';
 import { Product } from '@/types/product';
 
+// 이모티콘 정보를 가져온다.
 export async function fetchProductsInfo(username: string): Promise<Product[]> {
   const { data } = await instance.buyer.get(`${username}/products`);
 
   return data;
 }
 
+// 유저별 이모티큰 즐겨찾기 정보를 가져온다.
 export async function fetchProductByIsLike(
   username: string,
   id: number,
@@ -21,26 +23,37 @@ export async function fetchProductByIsLike(
   return data;
 }
 
+// 즐겨찾기 이모티콘 정보를 가져온다.
 export async function fetchFavoritesInfo(username: string) {
   const { data } = await instance.buyer.get(`${username}/products/favorite`);
 
   return data;
 }
 
+// 카테고리를 가져온다.
 export async function fetchCategories() {
   const { data } = await instance.buyer.get(`products/category`);
 
   return data;
 }
 
+// 인기순 이모티콘 정보를 가져온다.
 export async function fetchProductRank() {
   const { data } = await instance.buyer.get(`products/rank`);
 
   return data;
 }
 
+// 공지사항 리스트를 가져온다.
 export async function fetchNotices(page: number) {
   const { data } = await instance.buyer.get(`${page}/notices`);
+
+  return data;
+}
+
+// 특정 공지사항 정보를 가져온다.
+export async function fetchStoreNoticeInfo(id: number) {
+  const { data } = await instance.buyer.get(`notices/${id}`);
 
   return data;
 }
